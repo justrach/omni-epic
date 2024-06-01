34695f3117f682e68d5c61aa6de02a1fd9937404
Rach <54503978+justrach@users.noreply.github.com>
2024-05-31 21:49:56 -0700

    added wss

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -11,7 +11,7 @@ export function HomePage() {
             <p className="text-gray-900 dark:text-gray-400 font-bold">Be one of the first to dive into our interface. Sign up for a <Link href="/waitlist" className="text-purple-500 underline">timeslot</Link> and receive your unique access code.</p>
             <p className="text-gray-900 dark:text-gray-400">The game&apos;s resolution is optimized for smoother and faster rendering.</p>
             {/* <video className="w-full h-full object-cover rounded-md bg-gray-100 dark:bg-gray-800" controls> */}
-            <img src="http://localhost:3005/video_feed" alt="Game Stream"  className="w-full h-full border-rad object-cover rounded-md bg-gray-100 dark:bg-gray-800" />
+            <img src="https://app.p2w.app/video_feed" alt="Game Stream"  className="w-full h-full border-rad object-cover rounded-md bg-gray-100 dark:bg-gray-800" />
             {/* <img src="/placeholder.svg" alt="Game Stream"  className="w-full h-full border-rad object-cover rounded-md bg-gray-100 dark:bg-gray-800" /> */}
               Your browser does not support the video tag.
             {/* </video> */}
